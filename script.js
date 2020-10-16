fdf30a877a934856e749e266c0f491e713861c0f
Chandler Rogers <chandlerrogers@Chandlers-MacBook-Pro.local>
2020-10-16 09:50:29 -0600

    some styling changes and structuring response data for country search

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-/*
-document.getElementById("covidAPI").addEventListener("click", event => {
-    const url = "https://api.covid19api.com/summary";
-    fetch(url)
-        .then(response => {
-            return response.json();
-        }).then(json => {
-            console.log(json);
-            alert("New Confirmed Cases: " + json.Global.NewConfirmed);
-        });
-        
-});
-*/
-
 function getCovidData() {
     const url = "https://api.covid19api.com/summary";
     fetch(url)
@@ -19,36 +5,36 @@ function getCovidData() {
             return response.json();
         }).then(json => {
             console.log(json);
-            var summaryNewResults = "";
-            var summaryTotalResults = "";
+            let summaryNewResults = "";
+            let summaryTotalResults = "";
             summaryNewResults += "<div class='section-sub-container'>";
             summaryNewResults += "<h3 class='section-sub-header'>New Confirmed</h3>";
-            summaryNewResults += "<p>" + json.Global.NewConfirmed + "</p>";
+            summaryNewResults += "<p class='covid-summary'>" + json.Global.NewConfirmed + "</p>";
             summaryNewResults += "</div>";
 
             summaryNewResults += "<div class='section-sub-container'>";
             summaryNewResults += "<h3 class='section-sub-header'>New Deaths</h3>";
-            summaryNewResults += "<p>" + json.Global.NewDeaths + "</p>";
+            summaryNewResults += "<p class='covid-summary'>" + json.Global.NewDeaths + "</p>";
             summaryNewResults += "</div>";
 
             summaryNewResults += "<div class='section-sub-container'>";
             summaryNewResults += "<h3 class='section-sub-header'>New Recovered</h3>";
-            summaryNewResults += "<p>" + json.Global.NewRecovered + "</p>";
+            summaryNewResults += "<p class='covid-summary'>" + json.Global.NewRecovered + "</p>";
             summaryNewResults += "</div>";
 
             summaryTotalResults += "<div class='section-sub-container'>";
             summaryTotalResults += "<h3 class='section-sub-header'>Total Confirmed</h3>";
-            summaryTotalResults += "<p>" + json.Global.TotalConfirmed + "</p>";
+            summaryTotalResults += "<p class='covid-summary'>" + json.Global.TotalConfirmed + "</p>";
             summaryTotalResults += "</div>";
 
             summaryTotalResults += "<div class='section-sub-container'>";
             summaryTotalResults += "<h3 class='section-sub-header'>Total Deaths</h3>";
-            summaryTotalResults += "<p>" + json.Global.TotalDeaths + "</p>";
+            summaryTotalResults += "<p class='covid-summary'>" + json.Global.TotalDeaths + "</p>";
             summaryTotalResults += "</div>";
 
             summaryTotalResults += "<div class='section-sub-container'>";
             summaryTotalResults += "<h3 class='section-sub-header'>Total Recovered</h3>";
-            summaryTotalResults += "<p>" + json.Global.TotalRecovered + "</p>";
+            summaryTotalResults += "<p class='covid-summary'>" + json.Global.TotalRecovered + "</p>";
             summaryTotalResults += "</div>";
             document.getElementById("globalNewResults").innerHTML = summaryNewResults;
             document.getElementById("globalTotalResults").innerHTML = summaryTotalResults;
@@ -56,10 +42,13 @@ function getCovidData() {
         
 }
 
+// NOTE: Response data is structured differently depending on country -- countries with lots of provinces or states get all wonky
+// For best results, search Mexico, Madagascar, Guatemala, South Africa, Sweden, or any country really that doesn't have a bunch of provinces (China, Canada, U.S, etc.)
 function getCountryData() {
     const url = "https://api.covid19api.com/countries";
     const url2 = "https://api.covid19api.com/dayone/country/"
-    var countryName = document.getElementById("countryName").value;
+    let countryName = document.getElementById("countryName").value;
+    let results = "";
     fetch (url)
         .then(response => {
             return response.json();
@@ -77,6 +66,13 @@ function getCountryData() {
                     return response.json();
                 }).then(json => {
                     console.log(json);
-                })
-        })
+                    results += "<div class='section-sub-container'><h6>Covid Data in " + countryName + " ~ " + moment(json[json.length - 1].Date).format('MMMM Do YYYY') + "</h6><div class='section-sub-container'><ul>";
+                    results += "<li><span class='covid-summary'>Active: </span>" + json[json.length - 1].Active;
+                    results += "<li><span class='covid-summary'>Confirmed: </span>" + json[json.length - 1].Confirmed;
+                    results += "<li><span class='covid-summary'>Deaths: </span>" + json[json.length - 1].Deaths;
+                    results += "<li><span class='covid-summary'>Recovered: </span>" + json[json.length - 1].Recovered + "</ul></div>";
+                    results += "</div>";
+                    document.getElementById("countryResults").innerHTML = results;
+                });
+        });
 }
